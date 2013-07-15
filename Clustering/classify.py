f4d5e26e4922f5ddc6d4076ce31df4b65ebe5e61
Ricardo Lunar <ricardo4de9>
2013-07-15 16:23:30 -0430

    classify.py bug fixes

--- Clustering/classify.py
+++ Clustering/classify.py
@@ -2,103 +2,127 @@ import re
 import optparse
 
 def calculateSimilarity(setTrain,setTest):
-	sorted = 0.00
-	for example in setTrain:
-		if example in setTest:
-			sorted += 1.00
-	return 100*(sorted/(len(setTrain)))
+    sorted = 0.00
+    for example in setTrain:
+        if example in setTest:
+            sorted += 1.00
+    return 100*(sorted/(len(setTest)))
 
 def getSubsets(set,numclusters):
-	subsets = [[] for x in xrange(numclusters)]
-	subsetsWithClass = [[] for x in xrange(numclusters)]
+    subsets = [[] for x in xrange(numclusters)]
+    subsetsWithClass = [[] for x in xrange(numclusters)]
 
-	for i in range(1,numclusters+1):
-		for x in set:
-			if (int(x.split(",")[-1])==i):
-				example = re.sub(r',\d+\n','',x)
-				subsets[i-1].append(example)
-	return subsets 
+    for i in range(1,numclusters+1):
+        for x in set:
+            var = int(x.split(",")[-1])
+            if (var==i):
+                example = re.sub(r',\d+\n','',x)
+                subsets[i-1].append(example)
+    return subsets 
+
+def get_length(trainSubsets,testSubsets):
+    print len(trainSubsets[0])
+    print len(trainSubsets[1])
+    print len(trainSubsets[2])
+    print len(testSubsets[0])
+    print len(testSubsets[1])
+    print len(testSubsets[2])
+    
+def getFinalSimilarity(similarities,numclusters):
+    finalSimilarity = []
+    used = []
+    
+    for i in range(0,numclusters):
+        inicio = i*numclusters
+        fin = numclusters+(i*numclusters)
+        bestSimilar = -1;
+        indexSimilar = -1;
+        for j in range(inicio,fin):
+            if(similarities[j] > bestSimilar and j%numclusters not in used):
+                indexSimilar = j%numclusters
+                bestSimilar = similarities[j]
+        finalSimilarity.append(indexSimilar)
+        used.append(indexSimilar)
+    
+    return finalSimilarity
+                
 
 def getSimilarSubsets(trainSet,testSet,numclusters):
-	trainSubsets = getSubsets(trainSet,numclusters)
-	testSubsets = getSubsets(testSet,numclusters)
+    trainSubsets = getSubsets(trainSet,numclusters)
+    testSubsets = getSubsets(testSet,numclusters)
+    
+    similarities = []
 
-	similarities = []
+    for testSubset in testSubsets:
+        for trainSubset in trainSubsets:
+            similarity = calculateSimilarity(trainSubset,testSubset)
+            similarities.append(similarity)
+            
+    print similarities
+    
+    finalSimilarity = getFinalSimilarity(similarities,numclusters)
 
-	for trainSubset in trainSubsets:
-		for testSubset in testSubsets:
-			similarity = calculateSimilarity(trainSubset,testSubset)
-			similarities.append(similarity)
+    print finalSimilarity
 
-	finalSimilarity = []
-	for i in range(0,numclusters):
-		inicio = i*numclusters
-		fin = numclusters+(i*numclusters)
-		bestSimilar = -1;
-		indexSimilar = -1;
-		for j in range(inicio,fin):
-			if(similarities[j]>= bestSimilar):
-				indexSimilar = j%numclusters
-				bestSimilar = similarities[j]
-		finalSimilarity.append(indexSimilar)
+    finalTestSubsets = [[] for x in xrange(numclusters)]
+    for i in range(0,numclusters):
+        index = finalSimilarity[i];
+        finalTestSubsets[i] = testSubsets[index]
 
-	finalTestSubsets = [[] for x in xrange(numclusters)]
-	for i in range(0,numclusters):
-		index = finalSimilarity[i];
-		finalTestSubsets[i] = testSubsets[index]
-	return (trainSubsets,finalTestSubsets)
+    get_length(trainSubsets,testSubsets)
+    return (trainSubsets,finalTestSubsets)
 
 def calculatePrecision(training,testing):
-	correct = 0
-	incorrect = 0
-	for (tr,ot) in zip(training, testing):
-		for t in ot:
-			if t in tr:
-				correct += 1.00
-			else:
-				incorrect += 1.00
+    correct = 0
+    incorrect = 0
+    for (tr,ot) in zip(training, testing):
+        for t in ot:
+            if t in tr:
+                correct += 1.00
+            else:
+                incorrect += 1.00
 
-	print correct
-	print incorrect
+    print correct
+    print incorrect
 
-	print 100*(correct/(incorrect+correct))
+    print 100*(correct/(incorrect+correct))
 
 def main():
-	# parser for console input options
-	parser = optparse.OptionParser()
-	parser.add_option('-t', help='training data', type='string', dest='train_file')
-	parser.add_option('-o', help='clustering output', type='string', dest='out_file')
-	parser.add_option('-c', help='number of clusters', type='int', dest='numclusters')
-	#parser.add_option('-l', help='label to evaluate. if missing, test all', type='string', dest='label')
+    # parser for console input options
+    parser = optparse.OptionParser()
+    parser.add_option('-t', help='training data', type='string', dest='train_file')
+    parser.add_option('-o', help='clustering output', type='string', dest='out_file')
+    parser.add_option('-c', help='number of clusters', type='int', dest='numclusters')
+    #parser.add_option('-l', help='label to evaluate. if missing, test all', type='string', dest='label')
 
-	(opts, args) = parser.parse_args()
+    (opts, args) = parser.parse_args()
 
-	# Making sure all options appear
-	mandatories = ['train_file', 'out_file', 'numclusters']
-	for m in mandatories:
-		if not opts.__dict__[m]:
-			print "Mandatory option is missing\n"
-			parser.print_help()
-			exit(-1)
+    # Making sure all options appear
+    mandatories = ['train_file', 'out_file', 'numclusters']
+    for m in mandatories:
+        if not opts.__dict__[m]:
+            print "Mandatory option is missing\n"
+            parser.print_help()
+            exit(-1)
 
-	trnfile = open(opts.train_file, 'r')
-	outfile = open(opts.out_file, 'r')
-	numclusters = opts.numclusters
+    trnfile = open(opts.train_file, 'r')
+    outfile = open(opts.out_file, 'r')
+    numclusters = opts.numclusters
 
-	trainset = []
-	testset = []
-	comentario = re.compile(r'%.*(\n|$)')
+    trainset = []
+    testset = []
+    comentario = re.compile(r'%.*(\n|$)')
 
-	for t in trnfile:
-		trainset.append(t)
+    for t in trnfile:
+        trainset.append(t)
 
-	for o in outfile:
-		if (re.match(comentario, o) is None):
-			testset.append(o)
+    for o in outfile:
+        if (re.match(comentario, o) is None):
+            testset.append(o)
 
-	(training,testing) = getSimilarSubsets(trainset,testset,numclusters)
-
-	calculatePrecision(training,testing);
-
+    (training,testing) = getSimilarSubsets(trainset,testset,numclusters)
+    
+    calculatePrecision(training,testing)
+    
 if __name__ == "__main__":
-	main()
+    main()
